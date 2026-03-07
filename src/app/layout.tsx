69cbbf1ad0f8aa81bb037b3bd3149516e85bec51
Nicolay Camacho <nicolaycamacho@gmail.com>
2026-03-07 15:46:51 +0100

    Add Vercel analytics and speed libs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Providers from "@/app/providers";
 import GlobalCursor from "@/components/ui/GlobalCursor";
@@ -40,6 +42,8 @@ const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
       >
         <Providers>{children}</Providers>
         <GlobalCursor />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
